c2b5b4a824c20673f18b06bd57463bfa4b2f4af9
Brian Norman <brian.e.norman@gmail.com>
2016-12-15 22:59:26 -0600

    Convert from JAX-RS to Spring RestController
    Fixes #2

--- barkeep-service/src/main/java/com/bnorm/barkeep/Application.java
+++ barkeep-service/src/main/java/com/bnorm/barkeep/Application.java
@@ -1,13 +1,10 @@
 package com.bnorm.barkeep;
 
-import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.stereotype.Component;
 
-import com.bnorm.barkeep.service.BarkeepService;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootApplication
@@ -15,18 +12,6 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Component
-  public static class JerseyConfig extends ResourceConfig {
-
-    public JerseyConfig() {
-      registerEndpoints();
-    }
-
-    private void registerEndpoints() {
-      register(BarkeepService.class);
-    }
   }
 
   @Bean
--- barkeep-service/src/main/java/com/bnorm/barkeep/service/BarkeepService.java
+++ barkeep-service/src/main/java/com/bnorm/barkeep/service/BarkeepService.java
@@ -1,28 +1,28 @@
 package com.bnorm.barkeep.service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.bnorm.barkeep.db.BarkeepRepository;
 import com.bnorm.barkeep.model.Bar;
 import com.bnorm.barkeep.model.Book;
 import com.bnorm.barkeep.model.Recipe;
 import com.fasterxml.jackson.annotation.JsonView;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
-@Component
-@Path("/barkeep")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@RestController
+@RequestMapping("/barkeep")
 public class BarkeepService {
 
   private final BarkeepRepository repository;
@@ -40,58 +40,60 @@ public class BarkeepService {
   }
 
   @JsonView(Bar.class)
-  @GET
-  @Path("/bars")
+  @RequestMapping(value = "/bars")
   public List<Bar> getBars() {
     return repository.getUser(1).getBars();
   }
 
   @JsonView(Bar.class)
-  @GET
-  @Path("/bars/{id}")
-  public Bar getBar(@PathParam("id") long id) {
+  @RequestMapping(value = "/bars{id}")
+  public Bar getBar(@PathVariable("id") long id) {
     Bar bar = repository.getBar(id);
     if (bar == null) {
-      throw new NotFoundException("Unable to find bar with id:" + id);
+      throw new ResourceNotFoundException("Unable to find bar with id:%d", id);
     }
     return bar;
   }
 
   @JsonView(Book.class)
-  @GET
-  @Path("/books")
+  @RequestMapping("/books")
   public List<Book> getBooks() {
     return repository.getUser(1).getBooks();
   }
 
   @JsonView(Book.class)
-  @GET
-  @Path("/books/{id}")
-  public Book getBook(@PathParam("id") long id) {
+  @RequestMapping("/books/{id}")
+  public Book getBook(@PathVariable("id") long id) {
     Book book = repository.getBook(id);
     if (book == null) {
-      throw new NotFoundException("Unable to find book with id:" + id);
+      throw new ResourceNotFoundException("Unable to find book with id:%d", id);
     }
     return book;
   }
 
   @JsonView(Recipe.class)
-  @GET
-  @Path("/books/{bookId}/recipes")
-  public Map<Long, Recipe> getRecipes(@PathParam("bookId") long bookId) {
+  @RequestMapping("/books/{bookId}/recipes")
+  public Map<Long, Recipe> getRecipes(@PathVariable("bookId") long bookId) {
     Book book = getBook(bookId);
     return book.getRecipes();
   }
 
   @JsonView(Recipe.class)
-  @GET
-  @Path("/books/{bookId}/recipes/{recipeNumber}")
-  public Recipe getRecipe(@PathParam("bookId") long bookId, @PathParam("recipeNumber") long recipeNumber) {
+  @RequestMapping("/books/{bookId}/recipes/{recipeNumber}")
+  public Recipe getRecipe(@PathVariable("bookId") long bookId, @PathVariable("recipeNumber") long recipeNumber) {
     Book book = getBook(bookId);
     Recipe recipe = book.getRecipes().get(recipeNumber);
     if (recipe == null) {
-      throw new NotFoundException("Unable to find recipe with number:" + recipeNumber);
+      throw new ResourceNotFoundException("Unable to find recipe in book:%d with number:%d", bookId, recipeNumber);
     }
     return recipe;
+  }
+
+  @ResponseStatus(value = HttpStatus.NOT_FOUND)
+  public static class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String s, Object... args) {
+      super(String.format(s, args));
+    }
   }
 }
